51390f949c968454aab78a32e4f372aaab8f42c9
Ankita Patra <ankitapatra@Ankitas-MacBook-Air.local>
2024-04-18 20:27:15 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+//const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1b1b73eec9mshdd1fa8b1bb35df9p14ab53jsn48a35ddfab21',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "72204235a4b973b1564f551457f3af71";
